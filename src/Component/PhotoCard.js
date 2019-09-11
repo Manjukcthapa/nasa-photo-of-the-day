0b696d4a62396315c02e9cdb1420619e8dcea8bf
Manju KC <kcmanjuthapa@gmail.com>
2019-09-11 15:20:57 -0400

    fetched the data using axios

--- src/Component/PhotoCard.js
+++ src/Component/PhotoCard.js
@@ -7,7 +7,19 @@ const PhotoCard = () => {
     const [imgValue, setImgValue] = useState ("");
 
     useEffect(() => {
-    
+        //to request information from the server
+        axios.get(`https://api.nasa.gov/planetary/apod?api_key=74Cyf1i7aAfIDPJPSNGUt622Dn3c072942FuCQ17`)        
+        //if successful
+        .then(success => {
+            console.log('Success');
+            imgURl = success.data.url;
+            console.log(imgURl)
+            setImgValue(imgURl);
+        })
+        //if not successful
+        .catch(failure => {
+            console.log('Failure');
+        })
     }, [])
 
  
