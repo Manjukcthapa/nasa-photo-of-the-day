1796abb7ba52ff3ee855e331b6d13899bd5b2b57
Manju KC <kcmanjuthapa@gmail.com>
2019-09-11 14:53:54 -0400

    added state to the PhotoCard file

--- src/Component/PhotoCard.js
+++ src/Component/PhotoCard.js
@@ -1,2 +1,15 @@
 import React, {useEffect, useState} from "react";
 import axios from "axios";
+
+const PhotoCard = () => {
+    let imgURL= null;
+    const [imgValue, setImgValue] = useState ("");
+
+    return (
+        <div>
+            <h1>
+                Hello
+            </h1>
+        </div>
+    )
+}
