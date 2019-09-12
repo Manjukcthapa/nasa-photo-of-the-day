72aa593474b47af5beffa4a5155e5b0c1b9cca07
Manju KC <kcmanjuthapa@gmail.com>
2019-09-12 14:42:46 -0400

    styled imageList component

--- src/Component/Header.js
+++ src/Component/Header.js
@@ -1,32 +1,36 @@
-import React from 'react';
+import React from "react";
 import "./Nasapic.css";
 import styled from "styled-components";
 
 const Headers = styled.div`
-display: flex;
-flex-direction: row;
-flex-wrap: wrap;`
+  background-color: #51a0d5;
+  display: flex;
+  flex-direction: row;
+`;
 
 const Image = styled.img`
-height: 120px;
+  height: 120px;
   width: 140px;
-  margin-left: 50px;`
+  margin-left: 50px;
+`;
 
 const H1 = styled.h1`
-margin-left: 250px;
-margin-top:50px;
-color: white;
-`
+  margin-left: 250px;
+  margin-top: 50px;
+  color: white;
+`;
 
-function  Header(props) {
-    return (
-        <Headers className='header'>
-            <div className="img">
-            <Image  src="https://seeklogo.com/images/N/NASA-logo-9411797223-seeklogo.com.png" />
-            </div> 
-            <div><H1>NASA Photo of the Day!</H1></div>
-        </Headers>
-    )
+function Header(props) {
+  return (
+    <Headers className="header">
+      <div className="img">
+        <Image src="https://seeklogo.com/images/N/NASA-logo-9411797223-seeklogo.com.png" />
+      </div>
+      <div>
+        <H1>NASA Photo of the Day!</H1>
+      </div>
+    </Headers>
+  );
 }
 
 export default Header;
--- src/Component/ImageList.js
+++ src/Component/ImageList.js
@@ -1,9 +1,19 @@
 import React from "react";
+import styled from "styled-components";
+
+const Photo = styled.img`
+height: 500px;
+width: 600px;
+margin-left: 100px;
+margin-top: 70px;
+border: 10px solid transparent;
+`
+
 
 export default function ImageList(props) {
   return (
     <div>
-      <img src={props.src} alt={props.alt} />
+      <Photo src={props.src} alt={props.alt} />
     </div>
   );
 }
